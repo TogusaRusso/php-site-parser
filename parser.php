ecda68b41a65be66c789b8a895fd063937c12c40
Dmitry Akentyev <TogusaRusso@gmail.com>
2017-08-18 11:06:10 +0000

    Streamlined error handling

--- parser.php
+++ parser.php
@@ -7,25 +7,26 @@
   <input type='url' name='url'>
 </form>
 <?php
+
 $url = $_POST['url'];
 if ($url) {
   include('simple_html_dom.php');
-  if (url_valid($url)) {
-    // Create DOM from URL or file
-    $html = file_get_html($url);
-  
-    // Find all headers 
-    $headers = $html->find('h1');
-    if (count($headers)) {
-      foreach($html->find('h1') as $element) 
-        echo $element->innertext . '<br>';
-    } else {
-      die('Заголовков не найдено');
-    }
-  } else {
+
+  if (!url_valid($url)) 
     die('URL указывает на несуществующую страницу');
-  }  
+
+  // Create DOM from URL
+  $html = file_get_html($url);
+
+  // Find all headers 
+  $headers = $html->find('h1');
+  if (!count($headers)) 
+    die('Заголовков не найдено');
+  
+  foreach($html->find('h1') as $element) 
+    echo $element->innertext . '<br>';
 }
+
 function url_valid($url){
    $headers=get_headers($url);
    return stripos($headers[0],"200 OK")?true:false;
